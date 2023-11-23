cfa5d4a0f0cd4ffea9c20602bced1cc91f3aa067
Michael James Ignatoski <dev.dreadmap@gmail.com>
2023-11-23 03:35:46 -0500

    day21 pt 2progress

--- 2022/day21/main.go
+++ 2022/day21/main.go
@@ -13,6 +13,11 @@ type Monkey struct {
 	Type      bool
 	Op        rune
 	M1, M2    string
+	H         Human
+}
+
+type Human struct {
+	CoefN, ConstN, CoefD, ConstD int
 }
 
 func FindMonkey(name string, l []Monkey) *Monkey {
@@ -47,13 +52,49 @@ func (m *Monkey) GetNumber() int {
 
 var monkeys, monkeys2 []Monkey
 
+func (m *Monkey) GetHuman() Human {
+	if m.Name == "humn" {
+		return Human{1, 0, 1, 1}
+	}
+
+	if m.Type {
+		return m.H
+	}
+
+	m1, m2 := FindMonkey(m.M1, monkeys2), FindMonkey(m.M2, monkeys2)
+	fmt.Println(m1, m2)
+	h1, h2 := m1.GetHuman(), m2.GetHuman()
+	fmt.Println(h1, h2, string(m.Op))
+
+	switch m.Op {
+	case '+':
+		return Human{h1.CoefN + h2.CoefN, (h1.ConstN * h2.ConstD) + (h2.ConstN * h1.ConstD), h1.CoefD * h2.CoefD, h1.ConstD * h2.ConstD}
+	case '-':
+		return Human{h1.CoefN - h2.CoefN, (h1.ConstN * h2.ConstD) - (h2.ConstN * h1.ConstD), h1.CoefD * h2.CoefD, h1.ConstD * h2.ConstD}
+	case '*':
+		if h2.CoefN == 0 {
+			h2.CoefN = h2.ConstN
+		} else if h1.CoefN == 0 {
+			h1.CoefN = h1.ConstN
+		}
+		return Human{h1.CoefN * h2.CoefN, h1.ConstN * h2.ConstN, h1.CoefD * h2.CoefD, h1.ConstD * h2.ConstD}
+	case '/':
+		if h2.CoefN == 0 {
+			h2.CoefN = h2.ConstN
+		} else if h1.CoefN == 0 {
+			h1.CoefN = h1.ConstN
+		}
+		return Human{h1.CoefN * h2.CoefD, h1.ConstN * h2.ConstD, h1.CoefD * h2.CoefN, h1.ConstD * h2.ConstN}
+	}
+	panic("uh oh")
+}
 func init() {
 	monkeys = make([]Monkey, 0)
 	monkeys2 = make([]Monkey, 0)
 }
 
 func main() {
-	inputFile, _ := os.Open("input.txt")
+	inputFile, _ := os.Open("sample.txt")
 	defer inputFile.Close()
 	fileScanner := bufio.NewScanner(inputFile)
 
@@ -70,6 +111,7 @@ func main() {
 		} else {
 			m.Type = true
 			fmt.Sscanf(args[0], "%d", &m.Number)
+			m.H = Human{0, m.Number, 1, 1}
 		}
 
 		monkeys = append(monkeys, m)
@@ -81,5 +123,12 @@ func main() {
 	root := FindMonkey("root", monkeys)
 
 	fmt.Println("Part 1: ", root.GetNumber())
+
+	root2 := FindMonkey("root", monkeys2)
+	m1, m2 := FindMonkey(root2.M1, monkeys2), FindMonkey(root.M2, monkeys2)
+	h1, h2 := m1.GetHuman(), m2.GetHuman()
+
+	fmt.Println(h1, h2, m2)
+	fmt.Println("Part 2: ", ((h2.ConstN*h1.ConstD)-(h1.ConstN*h2.ConstD))/(h1.ConstD*h2.ConstD))
 
 }
