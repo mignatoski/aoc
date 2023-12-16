d0f5e34b6fa60163777e73452cd3541d250a40d2
Michael James Ignatoski <dev.dreadmap@gmail.com>
2023-12-15 21:54:32 -0500

    day 4 pt 2 done

--- 2023/day04/main.go
+++ 2023/day04/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,11 +15,14 @@ func main() {
 
 	var line string
 	var data, winning, mine []string
+	dups := make(map[int]int)
 	totalVal := 0
+	numCards := 0
 	for fileScanner.Scan() {
 		line = fileScanner.Text()
 		line = strings.ReplaceAll(line, "   ", " ")
 		line = strings.ReplaceAll(line, "  ", " ")
+		line = strings.ReplaceAll(line, ":", " :")
 		data = strings.Split(line, " ")
 
 		start, sep := 0, 0
@@ -30,21 +34,32 @@ func main() {
 				sep = i
 			}
 		}
+		id, _ := strconv.ParseInt(data[start-2], 0, 0)
 		winning = data[start:sep]
 		mine = data[sep+1:]
 
 		val := 0
+		winners := 0
 		for i := range winning {
 			for j := range mine {
 				if winning[i] == mine[j] {
+					winners++
 					val = max(2*val, 1)
 				}
 			}
 		}
-		fmt.Println(winning, mine, val)
+
+		for i := int(id) + 1; i <= int(id)+winners; i++ {
+			dups[i] += dups[int(id)] + 1
+		}
+
+		numCards += 1 + dups[int(id)]
+
+		fmt.Println(winning, mine, val, winners)
+
 		totalVal += val
 	}
 	fmt.Println("Part 1: ", totalVal)
 
-	fmt.Println("Part 2: ", line)
+	fmt.Println("Part 2: ", numCards)
 }
